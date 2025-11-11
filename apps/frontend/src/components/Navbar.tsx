0c863e7da929c7267f6ea0ac23814a822d3ea813
Aviprit Shah <avipritshah15@gmail.com>
2025-11-12 02:01:55 +0530

    navbar completed

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
     <nav className="h-[10%] bg-[#272727] p-4 flex items-center justify-between px-8">
       <h1 className=" text-white text-3xl font-bold">CaseFlow</h1>
       <div className="flex items-center">
-        <button className="text-white cursor-pointer">Login</button>
-        <button className="ml-4 text-white cursor-pointer">Sign Up</button>
+        <button className="px-3 py-1.5 bg-[#0e0e0e] text-white font-semibold rounded cursor-pointer border border-[#0e0e0e] hover:bg-[#1e1e1e]">Login</button>
+        <button className="ml-4 px-2.5 py-1 bg-gray-200 text-black font-semibold rounded cursor-pointer border border-gray-200 hover:bg-white/80">Sign Up</button>
       </div>
     </nav>
   );
