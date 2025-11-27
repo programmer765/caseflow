eb57229dd5b4c2c5cd45aeb6e3fa9ba0354cb1e2
Aviprit Shah <avipritshah15@gmail.com>
2025-11-27 23:37:09 +0530

    csv file display added on main component

--- apps/frontend/src/components/Main.tsx
+++ apps/frontend/src/components/Main.tsx
@@ -2,13 +2,18 @@
 
 
 import CSVUpload from "./CSVUpload";
-
+import { useCSVFileDataStore } from "@/store/CSVFileDataStore";
+import CSFileOpener from "./CSFileOpener";
 
 export default function Main() {
 
+  const data = useCSVFileDataStore((state) => state.data);
+
   return (
     <main className="h-[90%] bg-inherit p-4 flex flex-col justify-center items-center">
-      <CSVUpload />
+      {
+        data.length > 0 ? <CSFileOpener /> : <CSVUpload />
+      }
     </main>
   );
 }
